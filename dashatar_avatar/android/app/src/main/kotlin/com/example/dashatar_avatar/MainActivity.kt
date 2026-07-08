403de11e5f69ddcc525baef482dc0d2044b107d3
Shivraj1857 <shivraj.deshmukh@einfochips.com>
2026-07-08 14:58:20 +0530

    Dashatar app

---
+++ dashatar_avatar/android/app/src/main/kotlin/com/example/dashatar_avatar/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dashatar_avatar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- dashatar_avatar/test/widget_test.dart
+++ dashatar_avatar/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dahatar_app/main.dart';
+import 'package:dashatar_avatar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
