f3022d03d91ed28d8195ddf37e842667d46e4eeb
Shivraj1857 <shivraj.deshmukh@einfochips.com>
2026-07-15 17:14:54 +0530

    with BLoC pattern maximum pet mode screen Done

--- Braun_Metra_Demo/lib/bloc/pet_mode_bloc.dart
+++ Braun_Metra_Demo/lib/bloc/pet_mode_bloc.dart
@@ -1,9 +1,25 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 import 'pet_mode_event.dart';
 import 'pet_mode_state.dart';
+import '../utils/data_filter.dart';
+import '../utils/pet_mode_type.dart';
+
+PetModeType selectedType = PetModeType.scale;
 
 class PetModeBloc extends Bloc<PetModeEvent, PetModeState> {
-  PetModeBloc() : super(PetInitialState()) {
+  DateFilter selectedFilter = DateFilter.daily;
+
+  String dateRange = "";
+
+  PetModeBloc()
+    : super(
+        PetModeLoadedState(
+          selectedType: PetModeType.scale,
+          selectedFilter: DateFilter.daily,
+          dateRange: DateFormat("dd MMM").format(DateTime.now()).toUpperCase(),
+        ),
+      ) {
     on<StartWeightCaptureEvent>((event, emit) {
       emit(NavigateScreen2State());
     });
@@ -23,5 +39,75 @@ class PetModeBloc extends Bloc<PetModeEvent, PetModeState> {
     on<WeighAgainEvent>((event, emit) {
       emit(NavigateScreen1State());
     });
+
+    on<ChangeMeasureTypeEvent>(_changeMeasure);
+
+    on<ChangeDateFilterEvent>(_changeFilter);
+  }
+
+  Future<void> _changeMeasure(
+    ChangeMeasureTypeEvent event,
+    Emitter<PetModeState> emit,
+  ) async {
+    selectedType = event.type;
+
+    emit(
+      PetModeLoadedState(
+        selectedType: selectedType,
+        selectedFilter: selectedFilter,
+        dateRange: dateRange,
+      ),
+    );
+  }
+
+  Future<void> _changeFilter(
+    ChangeDateFilterEvent event,
+    Emitter<PetModeState> emit,
+  ) async {
+    selectedFilter = event.filter;
+
+    updateDateRange(selectedFilter);
+
+    emit(
+      PetModeLoadedState(
+        selectedType: selectedType,
+        selectedFilter: selectedFilter,
+        dateRange: dateRange,
+      ),
+    );
+  }
+
+  void updateDateRange(DateFilter filter) {
+    final DateTime today = DateTime.now();
+
+    final formatter = DateFormat("dd MMM").format;
+
+    switch (filter) {
+      case DateFilter.daily:
+        dateRange = formatter(today).toUpperCase();
+        break;
+
+      case DateFilter.weekly:
+        final start = today.subtract(const Duration(days: 6));
+        dateRange =
+            "${formatter(start).toUpperCase()} - ${formatter(today).toUpperCase()}";
+        break;
+
+      case DateFilter.month30:
+        final start = today.subtract(const Duration(days: 29));
+        dateRange =
+            "${formatter(start).toUpperCase()} - ${formatter(today).toUpperCase()}";
+        break;
+
+      case DateFilter.year1:
+        final start = DateTime(today.year - 1, today.month, today.day);
+
+        final yearFormatter = DateFormat("dd MMM yyyy").format;
+
+        dateRange =
+            "${yearFormatter(start).toUpperCase()} - ${yearFormatter(today).toUpperCase()}";
+
+        break;
+    }
   }
 }
--- Braun_Metra_Demo/lib/bloc/pet_mode_state.dart
+++ Braun_Metra_Demo/lib/bloc/pet_mode_state.dart
@@ -1,3 +1,6 @@
+import '../utils/data_filter.dart';
+import '../utils/pet_mode_type.dart';
+
 abstract class PetModeState {}
 
 class PetInitialState extends PetModeState {}
@@ -11,3 +14,15 @@ class NavigateScreen4State extends PetModeState {}
 class WeightSavedState extends PetModeState {}
 
 class NavigateScreen1State extends PetModeState {}
+
+class PetModeLoadedState extends PetModeState {
+  final PetModeType selectedType;
+  final DateFilter selectedFilter;
+  final String dateRange;
+
+   PetModeLoadedState({
+    required this.selectedType,
+    required this.selectedFilter,
+    required this.dateRange,
+  });
+}
